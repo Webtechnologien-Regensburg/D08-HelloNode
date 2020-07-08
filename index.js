28970ec2bf61567f9b71f21da7c035b9d1c29d4c
Alexander Bazo <alexanderbazo@googlemail.com>
2020-07-08 13:20:41 +0200

    Fix typos

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+/**
+ * Diese Datei enthält den Code eines einfachen Hello World-Programms für Node.js
+ * 
+ * Zum Ausführen des Programms gehen Sie so vor:
+ * 
+ * - Installieren Sie Node.js über den heruntergeladenen Installer
+ * - Starten Sie die Node.js-Kommandozeile und navigieren Sie zum Ordner mit dieser Datei*
+ * - Rufen Sie in der Kommandozeile node index.js auf
+ * 
+ *  *: Wenn Sie mit dem Editor Visual Studio Code arbeiten, können Sie den Projektordner im Editor öffnen,
+ *  im Kontextmenü der Datei index.js den Eintrag "Open in Terminal" auswählen und befinden sich in der 
+ *  Kommandozeile, die sich unter dem Editor geöffnet hat direkt im Projektordner.
+ */
+
+// Diese Funktion gibt "Hello Node!" auf der Standardausgabe des Systems aus
 function sayHello() {
-	console.log("Hello Node!");
+    console.log("Hello Node!");
 }
 
 sayHello();
